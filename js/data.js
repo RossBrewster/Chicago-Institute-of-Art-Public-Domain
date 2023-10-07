114491f207ab1d31412d5f94df95171781486d6f
Ross Brewster <ross.brewster14@gmail.com>
2023-10-07 21:22:29 +0000

    Change like button to red when clicked, and trims unfavorited artworks from data array before storing in local storage. #3

--- js/data.js
+++ js/data.js
@@ -1,2 +1,18 @@
 /* exported data */
-const data = [];
+let data = [];
+const previouslyFavorited = localStorage.getItem('favorites');
+
+if (previouslyFavorited !== null) data = JSON.parse(previouslyFavorited);
+
+window.addEventListener('beforeunload', handleBeforeUnload);
+
+function handleBeforeUnload(event) {
+  const favorites = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].isFavorited === true) {
+      favorites.push(data[i]);
+    }
+  }
+  const favoritesJSON = JSON.stringify(favorites);
+  localStorage.setItem('favorites', favoritesJSON);
+}
